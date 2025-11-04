9f2b25d1c1c636c816b7de83fcf93fa4dd32faed
blelian <bullsurus@gmail.com>
2025-11-04 23:33:55 +0200

    modified stocks.module

--- quantmath/apps/gateway/src/stocks/stocks.module.ts
+++ quantmath/apps/gateway/src/stocks/stocks.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StocksService } from './stocks.service';
+import { StockEntity } from './stock.entity';
 import { StocksController } from './stocks.controller';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  controllers: [StocksController],
+  imports: [
+    TypeOrmModule.forFeature([StockEntity]), // <-- this makes the repository injectable
+    ScheduleModule.forRoot(), // if you want cron jobs
+  ],
   providers: [StocksService],
+  controllers: [StocksController],
+  exports: [StocksService],
 })
 export class StocksModule {}
