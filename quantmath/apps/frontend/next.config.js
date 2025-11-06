07ea6d4cc75cf3bd8616125fb72c36075d55d9d0
blelian <bullsurus@gmail.com>
2025-11-07 00:19:34 +0200

    main.py for local docker testing

--- quantmath/apps/frontend/next.config.js
+++ quantmath/apps/frontend/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: false, // disable Turbopack
-  },
+  reactStrictMode: true, 
+  swcMinify: true,       
 };
 
 export default nextConfig;
